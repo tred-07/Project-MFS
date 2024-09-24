e9b9702a9e04c6c6248e1b383ff2b12453413101
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-24 18:40:03 +0600

    Update

--- JS/log_in.js
+++ JS/log_in.js
@@ -1,12 +1,17 @@
 console.log("log_in.js connected");
-document.getElementById('login-button').addEventListener('click',function(event){
+document.getElementById('login-button').addEventListener('click', function (event) {
     console.log("Button Click");
     event.preventDefault();//prevent refresh of page
-    const phoneNumber=document.getElementById('account-number').value;
+    const phoneNumber = document.getElementById('account-number').value;
     console.log(phoneNumber);
-    const password=document.getElementById('password').value;
+    const password = document.getElementById('password').value;
     console.log(password);
-    if(password==='1234')console.log('Okay');
-    else console.log('Not Okay');
-
+    if (password === '1234' && phoneNumber.length==11) {
+        console.log('Okay');
+        window.location.href = './test.html';//redirect another webpage
+    }
+    else {
+        alert('Enter wrong credential');
+        console.log('Not Okay');
+    }
 })
