66909f0c344460599e7144a19a0b8cc716250401
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-24 22:43:28 +0600

    clock customize

--- JS/copyright.js
+++ JS/copyright.js
@@ -1,3 +1,5 @@
 const date = new Date();
 const formattedDate = date.toLocaleString('en-US', { timeZoneName: 'short' });
 document.getElementById('copyright').innerText="All right reserved by tred-07 © 2022 - "+formattedDate;
+document.getElementById('copyright').style.color='brown'
+document.getElementById('copyright').style.fontSize='25px'
