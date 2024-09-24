b53f63f0adf1b77232b4311043019564210910fd
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-24 22:32:31 +0600

    add copyright

---
+++ JS/copyright.js
@@ -0,0 +1,3 @@
+const date = new Date();
+const formattedDate = date.toLocaleString('en-US', { timeZoneName: 'short' });
+document.getElementById('copyright').innerText="All right reserved by tred-07 © 2022- "+date.toDateString();
